f50b889b638cfad6399027f2aa584f0ca657786a
Artem Chupylko <achupylko@gmail.com>
2026-01-05 12:44:37 +0000

    Add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
 
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+  } else {
+    console.log({
+      email,
+      password,
+    });
+    form.reset();
+  }
+});
