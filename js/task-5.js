ed7f1d42b963f2a63a9f104eb53de86c5c0cb7de
Artem Chupylko <achupylko@gmail.com>
2026-01-05 13:03:28 +0000

    Add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorText = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorText.textContent = color;
+});
